b2df55e55e2f0dc7b749fc19cd0deb5a513be47c
russelladams <russell.adams2014@gmail.com>
2013-08-27 12:53:04 -0500

    Update handeval.py

--- handeval.py
+++ handeval.py
@@ -10,18 +10,19 @@ Created on Fri Aug 23 05:17:40 2013 #
 """
 
 import cProfile
+from collections import deque
 
 def handtype(card1,card2,flop1=(-12,"a"),flop2=(-22,"b"),
              flop3=(-32,"z"),turn=(-42,"d"),river=(-52,"e")):
     '''Returns the hand type of the given hand. Royal Flush, Straight
-       Flush, Four of a Kind, Fullhouse, Flush, Straight, Three of a
-       Kind, Two Pair, Pair, High Card.'''
+Flush, Four of a Kind, Fullhouse, Flush, Straight, Three of a
+Kind, Two Pair, Pair, High Card.'''
     # Putting each card (Value, Suit) into a list.
     temp = [card1,card2,flop1,flop2,flop3,turn,river]
-    # Adds, each card(by value) to the cards dictionary. If the 
+    # Adds, each card(by value) to the cards dictionary. If the
     # card is already in the dictionary, it instead appends the suit
     # to the existing key value of the dictionary.
-    best_hand = []
+    best_hand = deque()
     cards = {}
     for card in temp:
         if card[0] > 0:
@@ -33,13 +34,13 @@ def handtype(card1,card2,flop1=(-12,"a"),flop2=(-22,"b"),
     # Checks if 10,J,Q,K,A are in the cards dictionary.
     # Then checks to see if their suits all match.
     hand_type = None
-    if (1 in cards) & (10 in cards) & (11 in cards)& (12 in cards) & (13 in cards):
-        if (cards[1]==cards[10]) & (cards[1]==cards[11]) & (cards[1]==cards[12]) & (cards[1]==cards[13]):
+    if (14 in cards) & (10 in cards) & (11 in cards)& (12 in cards) & (13 in cards):
+        if (cards[14]==cards[10]) & (cards[14]==cards[11]) & (cards[14]==cards[12]) & (cards[14]==cards[13]):
             return "Royal Flush",10,best_hand
         else:
             hand_type = "Straight"
     # Straight Flush
-    # Iterates over each key in cards dictionary. And checks to 
+    # Iterates over each key in cards dictionary. And checks to
     # see if each key value from +1 to +4 exists after that key.
     # Then checks to see if their suits all match
     for key,value in cards.iteritems():
@@ -47,13 +48,13 @@ def handtype(card1,card2,flop1=(-12,"a"),flop2=(-22,"b"),
             if (cards[key]==cards[key+1]) & (cards[key]==cards[key+2]) & (cards[key]==cards[key+3]) & (cards[key]==cards[key+4]):
                 return "Straight Flush",9,best_hand
             else:
-                hand_type = "Straight"               
+                hand_type = "Straight"
     # Four of a kind
     # Iterates over each key in the cards dictionary.
     # Then checks to see if the key value's length is 4.
-    # i.e  dict = {7:'s','c','h','d'}
+    # i.e dict = {7:'s','c','h','d'}
     # len(dict[7]) = 4
-    best_hand = []
+    best_hand.clear()
     tmp = []
     cardsCopy = cards.copy()
     for key,value in cards.iteritems():
@@ -75,7 +76,7 @@ def handtype(card1,card2,flop1=(-12,"a"),flop2=(-22,"b"),
     # If it is, it deletes that key from the dictionary copy.
     # Then iterates over the copied dictionary again to see if another
     # key value is greater than or equal to 2.
-    best_hand = []    
+    best_hand.clear()
     x = cards.copy()
     for key,value in cards.iteritems():
         if len(value) == 3:
@@ -92,11 +93,11 @@ def handtype(card1,card2,flop1=(-12,"a"),flop2=(-22,"b"),
                             best_hand.append((14,i))
                     for j in value2:
                         best_hand.append((key2,value2))
-                    return "Fullhouse",7,best_hand               
+                    return "Fullhouse",7,best_hand
     # Flush
     # Iterates over each key value in the cards dictionary.
     # Keeps track of the suits and returns Flush if it finds 5 or more.
-    '''Need to create a best_hand return for flushes'''
+    best_hand.clear()
     clubs = 0
     spades = 0
     diamonds = 0
@@ -110,88 +111,75 @@ def handtype(card1,card2,flop1=(-12,"a"),flop2=(-22,"b"),
             diamonds+=1
         if cards[key] == ['h']:
             hearts+=1
-    if (clubs >= 5): 
-        for key in cards:
-            if cards[key] == 'c':
-                best_hand.append(key)
-        return "Flush",6, best_hand.sort()
+    if (clubs >= 5):
+        for key,value in cards.iteritems():
+            for i in value:
+                if i == 'c':
+                    best_hand.append(key)
+        return "Flush",6, best_hand
     if (spades >= 5):
-        for key in cards:
-            if cards[key] == 's':
-                best_hand.append(key)
-        return "Flush",6, best_hand.sort()
-    if (diamonds >= 5): 
-        for key in cards:
-            if cards[key] == 'd':
-                best_hand.append(key)
-        return "Flush",6, best_hand.sort()
+        for key,value in cards.iteritems():
+            for i in value:
+                if i == 's':
+                    best_hand.append(key)
+        return "Flush",6, best_hand
+    if (diamonds >= 5):
+        for key,value in cards.iteritems():
+            for i in value:
+                if i == 'd':
+                    best_hand.append(key)
+        return "Flush",6, best_hand
     if (hearts >= 5):
-        for key in cards:
-            if cards[key] == 'h':
-                best_hand.append(key)
-        return "Flush",6, best_hand.sort()
+        for key,value in cards.iteritems():
+            for i in value:
+                if i == 'h':
+                    best_hand.append(key)
+        return "Flush",6, best_hand
     # Three of a kind
     # Iterates over each key value in the cards dictionary.
     # If the length of any value is 3, it returns Three of a Kind.
-    best_hand = []
-    tmp = []    
+    best_hand.clear()
+    temp = []
     cardsCopy = cards.copy()
     for key,value in cards.iteritems():
         if len(value) == 3:
-            if key == 1:
-                best_hand.append(14)
-            else:
-                best_hand.append(key)
+            best_hand.append(key)
             del cardsCopy[key]
             for key2,value2 in cardsCopy.iteritems():
-                tmp.append(key2)
-            tmp.sort()
-            best_hand.append(tmp[-1])
-            best_hand.append(tmp[-2])
+                temp.append(key2)
+            temp.sort()
+            best_hand.append(temp[-1])
+            best_hand.append(temp[-2])
             return "Three of a Kind",5,best_hand
-           
     # Two Pair
-    best_hand = []
-    tmp = []
+    best_hand.clear()
+    temp = []
     cardsCopy = cards.copy()
-    for key, value in cardsCopy.iteritems():
+    for key, value in cards.iteritems():
         if len(value) == 2:
-            if key == 1:
-                best_hand.append(15)
-            else:
-                best_hand.append(key)
-        else:
-            if key == 1:
-                best_hand.append(14)
-            else:
-                tmp.append(key)
-    if len(best_hand) == 3:
-        best_hand.sort()
-        del best_hand[0]
-        best_hand.append(tmp[0])
-        return "Two Pair",3,best_hand
-    if len(best_hand) == 2:
-        best_hand.sort()
-        best_hand.append(tmp[2])
+            best_hand.append(key)
+            del cardsCopy[key]
+    if len(best_hand) >= 2:
+        for key in cardsCopy.iterkeys():
+            tmp.append(key)
+        best_hand.append(tmp[-1])
         return "Two Pair",3,best_hand
     # Pair
-    best_hand = []
-    tmp = []
+    best_hand.clear()
+    temp = []
     cardsCopy = cards.copy()
     for key,value in cards.iteritems():
         if len(value) == 2:
             best_hand.append(key)
             del cardsCopy[key]
-            for key in cardsCopy.iterkeys():
-                tmp.append(key)
     if len(best_hand) == 1:
-        best_hand.append(tmp[4])
-        best_hand.append(tmp[3])
-        best_hand.append(tmp[2])
+        for key in cardsCopy.iterkeys():
+            temp.append(key)
+        best_hand.append(max(temp))
         return "One Pair",2,best_hand
 
     # High Card
-    best_hand = []
+    best_hand.clear()
     if hand_type:
         for key in cards.iterkeys():
             if (key+1 in cards) and (key+2 in cards) and (key+3 in cards) and (key+4 in cards):
@@ -214,9 +202,9 @@ def handtype(card1,card2,flop1=(-12,"a"),flop2=(-22,"b"),
         return "High Card",1, best_hand
 
 def evaluate(card1,card2,card3,card4,board):
-    '''Evaluate one hand vs another.'''       
+    '''Evaluate one hand vs another.'''
     # Create flops for the hands and put them in a list
-    # The first hand is (c1,s1),(c2,s2) = (card one,suit one),(card two, suit two)  
+    # The first hand is (c1,s1),(c2,s2) = (card one,suit one),(card two, suit two)
     test1 = [card1,card2]
     test2 = [card3,card4]
     for x in board:
@@ -235,9 +223,9 @@ def evaluate(card1,card2,card3,card4,board):
         if (Hand1[2][0] == 1) and (Hand2[2][0] == 1):
             del(Hand1[2][0])
             del(Hand2[2][0])
-        if Hand1[2] > Hand2[2]:
+        if max(Hand1[2]) > max(Hand2[2]):
             return 1
-        if Hand1[2] < Hand2[2]:
+        if max(Hand1[2]) < max(Hand2[2]):
             return 2
         else:
             print("high card")
@@ -245,51 +233,60 @@ def evaluate(card1,card2,card3,card4,board):
             print test1, '-->',test2
             return 0
     if (Hand1[0] == "One Pair"):
-        if (Hand1[2][0] == 1) and (Hand2[2][0] == 1):
-            del(Hand1[2][0])
-            del(Hand1[2][0])
-            if (Hand1[2][0] > Hand2[2][0]):
+        if Hand1[2][0] > Hand2[2][0]:
+            return 1
+        if Hand2[2][0] > Hand1[2][0]:
+            print("Hand 2!")
+            print Hand1[2]
+            print Hand2[2]
+            return 2
+        if Hand1[2][0] == Hand2[2][0]:
+            del Hand1[2][0]
+            del Hand2[2][0]
+            if max(Hand1[2]) > max(Hand2[2]):
                 return 1
-            if (Hand1[2][0] < Hand2[2][0]):
+            if max(Hand2[2]) > max(Hand1[2]):
                 return 2
-            else:
-                return 0
-        if (Hand1[2][0] == 1):
-            return 1
-        if (Hand2[2][0] == 1):
-            return 2
-        if Hand1[2] > Hand2[2]:
-            return 1
-        if Hand1[2] < Hand2[2]:
-            return 2
         else:
-            return 0
+            print("tie at one pair")
     # Now checking two pair combinations to see which is better.
-    elif (Hand1[0] == "Two Pair"):
-        if (Hand1[2] > Hand2[2]):
+    if (Hand1[0] == "Two Pair"):
+        hand_1 = []
+        hand_2 = []
+        hand_1.append(Hand1[2][0])
+        hand_1.append(Hand1[2][1])
+        hand_2.append(Hand2[2][0])
+        hand_2.append(Hand2[2][1])
+        
+        if (max(hand_1) > max(hand_2)):
             return 1
-        if (Hand1[2] < Hand2[2]):
+        if (max(hand_2) > max(hand_1)):
             return 2
-        else:
-            return 0
+        if Hand1[2][-2] > Hand2[2][-2]:
+            return 1
+        if Hand2[2][-2] > Hand1[2][-2]:
+            return 2
                     
     # Now checking Three of a Kind combinations to see which is better.
-    elif (Hand1[0] == "Three of a Kind"):
-        if (Hand1[2] > Hand2[2]):
+    if (Hand1[0] == "Three of a Kind"):
+        if (Hand1[2][0] > Hand2[2][0]):
             return 1
-        if (Hand1[2] < Hand2[2]):
+        if (Hand1[2][0] < Hand2[2][0]):
             return 2
         else:
             return 0
     
     # Need to do one for Flushes
-    elif (Hand1[0] == "Flush"):
-        # This is working already.
-        # I'm not sure how.
-        pass
+    if (Hand1[0] == "Flush"):
+        if max(Hand1[2]) > max(Hand2[2]):
+            return 1
+        if max(Hand2[2]) > max(Hand1[2]):
+            return 2
+        else:
+            return 0
     
     # Need to do one for Straights
-    elif (Hand1[0] == "Straight"):
+    if (Hand1[0] == "Straight"):
         if (Hand1[2][4] > Hand2[2][4]):
             return 1
         if (Hand1[2][4] < Hand2[2][4]):
@@ -298,14 +295,12 @@ def evaluate(card1,card2,card3,card4,board):
             return 0
     
     # Now checking Fullhouses
-    elif (Hand1[0] == "Fullhouse"):
+    if (Hand1[0] == "Fullhouse"):
         if (Hand1[2][2][0] == 1) and (Hand2[2][2] == 1):
             if (Hand1[2][3] > Hand2[2][3]):
                 return 1
             if (Hand1[2][3] < Hand2[2][3]):
                 return 2
-            else:
-                return 0
         if (Hand1[2][2] == 1):
             return 1
         if (Hand2[2][2] == 1):
@@ -317,7 +312,7 @@ def evaluate(card1,card2,card3,card4,board):
         else:
             return 0
     # Now checking Four of a Kinds
-    elif (Hand1[0] == "Four of a Kind"):
+    if (Hand1[0] == "Four of a Kind"):
         if (Hand1[2][0] == Hand2[2][0]):
             if (Hand1[2][1] > Hand2[2][1]):
                 return 1
@@ -329,7 +324,3 @@ def evaluate(card1,card2,card3,card4,board):
             return 2
         else:
             return 0
-        
-
-    else:
-        return 0
