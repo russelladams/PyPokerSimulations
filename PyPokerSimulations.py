2748327d63f04972e6d8e217e87cd9b24056b27b
russelladams <russell.adams2014@gmail.com>
2013-08-30 01:51:49 -0500

    Update PyPokerSimulations.py

--- PyPokerSimulations.py
+++ PyPokerSimulations.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 ###################################################
 Created on Fri Aug 23 05:17:40 2013               #
@@ -9,12 +8,12 @@ Created on Fri Aug 23 05:17:40 2013               #
 
 
 """
-import cProfile
+#import cProfile
 
 import random
 from math import factorial
 from itertools import combinations
-from handeval import handtype,evaluate
+from handeval import evaluate
 
 def combin(n,k):
     '''Combination function. For example, if you wanted to know how many
@@ -32,25 +31,27 @@ def createdeck():
             deck.append((value,suit))
     return deck
     
-def createboards(card1,card2,card3,card4,flop1=(None,None),flop2=(None,None),flop3=(None,None)):
+def createboards(card1,card2,card3=None,card4=None,flop1=(None,None),flop2=(None,None),flop3=(None,None)):
     '''Returns all possible 5 card boards for 2-4 hole cards. You can also
        specify a flop, and instead create all possible turn/river cards.'''
     deck = createdeck()
     # Remove the four specified cards from the deck.
     deck.remove(card1)
     deck.remove(card2)
-    deck.remove(card3)
-    deck.remove(card4)
+    if card3:
+        deck.remove(card3)
+        deck.remove(card4)
     # Check to see if a flop was specified. If so, remove those 3 from the deck.
     if flop1 != (None,None):
         deck.remove(flop1)
         deck.remove(flop2)
         deck.remove(flop3)
         c = list(combinations(deck,2))
+        return c
         boards = {}
         for j in range(len(c)):
             boards[j] = c[j]
-            return boards
+        return boards
     # Using itertools.combinations we create all 5 card combinations from
     # the remaining cards in the deck. Then place each of those
     # combinations into a dictionary for quick lookup.        
@@ -60,18 +61,22 @@ def createboards(card1,card2,card3,card4,flop1=(None,None),flop2=(None,None),flo
         boards[j] =c[j]
     return boards
         
-def randomrange(card1,card2):
+def randomrange(card1,card2,flop=None):
     '''Creates a random range for an opponent'''
     deck = createdeck()
     deck.remove(card1)
     deck.remove(card2)
+    if flop != None:
+        deck.remove(flop[0])
+        deck.remove(flop[1])
+        deck.remove(flop[2])
     c = list(combinations(deck,2))
     hands = {}
     for i in range(len(c)):
         hands[i] = c[i]
     return hands
 
-def equity(card1,card2,card3,card4,simulations=30000):
+def equity(card1,card2,card3,card4,simulations=70000):
     '''Equates the equity of one specified hand,
        versus another specified hand. Monte Carlo style.'''
     one = 0.0
@@ -101,31 +106,48 @@ def equity(card1,card2,card3,card4,simulations=30000):
     print("Two is winning ",float(two/total))
     print("And they are tying ",float(tie/total))
     
-def equityVsrandom(card1,card2,simulations=30000):
+def equityVsrandom(card1,card2,flop=None,simulations=10000):
     '''Equates the equity of one specified hand, versus another 
        Random Hand.'''
     ranrange = randomrange(card1,card2)
-    card3,card4 = random.choice(ranrange)
     one = 0.0
     two = 0.0
     tie = 0.0
-    flops = createboards(card1,card2,card3,card4)
-    for i in range(simulations):
-        flop = random.choice(flops)
-        x = evaluate(card1,card2,card3,card4,flop)
-        if x == 1:
-            one+=1.0
-        elif x == 2:
-            two+=1.0
-        else:
-            tie+=1.0
+    if flop == None:
+        flops = createboards(card1,card2)
+        for i in range(simulations):
+            flop = random.choice(flops)
+            card3,card4 = random.choice(ranrange)
+            x = evaluate(card1,card2,card3,card4,flop)
+            if x == 1:
+                one+=1.0
+            elif x == 2:
+                two+=1.0
+            else:
+                tie+=1.0
+    else:
+        flops = createboards(card1,card2,card3=None,card4=None,flop1=flop[0],
+                             flop2=flop[1],flop3=flop[2])
+        for i in range(simulations):
+            card3,card4 = random.choice(ranrange)
+            x = random.choice(flops)
+            x = evaluate(card1,card2,card3,card4,random.choice(flops)+flop)
+            if x == 1:
+                one+=1.0
+            elif x == 2:
+                two+=1.0
+            else:
+                tie+=1.0
+                
     total = one+two+tie
-    print total
-    print("One is winning ",float(one/total))
-    print("Two is winning ",float(two/total))
-    print("And they are tying ",float(tie/total))
-        
-#cProfile.run("equityVsrandom((1,'c'),(1,'s'))")    
-#cProfile.run("equity((1,'c'),(1,'s'),(13,'c'),(12,'s'),50000)")
-#equityVsrandom((1,'c'),(1,'s'))
-#equity((14,'c'),(14,'s'),(13,'c'),(13,'s'),40000)
+    results = (one/total*100),(two/total*100),(tie/total*100)
+    return results
+    
+
+if __name__ == '__main__':      
+    #cProfile.run("equityVsrandom((14,'c'),(14,'s'))")    
+    #cProfile.run("equity((14,'c'),(14,'s'),(13,'c'),(12,'s'),10000)")
+    #equityVsrandom((14,'c'),(14,'s'))
+    print equityVsrandom((3,'c'),(4,'s'),flop=((9,'c'),(7,'d'),(10,'c')))
+
+    
